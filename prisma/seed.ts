3bf4194ffbc235c4b0704829af6916c454628fc3
montasergohar <montasergohar@gmail.com>
2024-02-17 20:24:16 +0200

    Create 25 Record using Faker

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-
+import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.create({
-        data:{
-            email:"montaser@gmail.com"
-        }
-    })
+  await prisma.user.createMany({
+    data: Array.from({ length: 25 }, () => ({
+      email: faker.internet.email(),
+      name: faker.internet.userName(),
+      address: {
+        street: faker.location.street(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zip: faker.location.zipCode(),
+      },
+    })),
+  });
 }
 
 main()
