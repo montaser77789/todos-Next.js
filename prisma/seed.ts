ca901f6799f2116d51cdc68c2df2fa0259f972bb
montasergohar <montasergohar@gmail.com>
2024-02-17 19:47:59 +0200

    Create data base using Mongidb && prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data:{
+            email:"montaser@gmail.com"
+        }
+    })
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
